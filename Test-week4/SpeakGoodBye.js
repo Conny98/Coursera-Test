3848efb561cdc058a532dc36d8dee6c14ae4d03f
Conny98 <64207309+Conny98@users.noreply.github.com>
2020-11-07 14:17:16 +0100

    Update SpeakGoodBye.js

--- Test-week4/SpeakGoodBye.js
+++ Test-week4/SpeakGoodBye.js
@@ -7,7 +7,7 @@
 // See Lecture 52, part 2
 
 var byeSpeaker = {};
-byeSpeaker.speak= "Frank"
+
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
@@ -18,8 +18,8 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.Greet = function () {
-  console.log(speakWord + " " + byeSpeaker.speak);
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
